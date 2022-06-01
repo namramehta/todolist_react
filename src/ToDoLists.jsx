54e69e39c58c8b7283af31893a3f3fa79090d62f
Namra <namramehta108@gmail.com>
2022-06-01 15:01:21 +0530

    initial commit

---
+++ src/ToDoLists.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const ToDoLists = (props) => {
+  return (
+    <>
+      <div className="todo_style">
+        <i
+          class="fa fa-items"
+          aris-hidden="true"
+          onClick={() => {
+            props.onSelect(props.id);
+          }}
+        />
+        <li>{props.text}</li>
+      </div>
+    </>
+  );
+};
+export default ToDoLists;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+
+<script
+  src="https://kit.fontawesome.com/83d04410b7.js"
+  crossorigin="anonymous"
+></script>;
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
